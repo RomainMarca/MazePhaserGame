1e2e945c548e47035a18423f113c9114fe90f9b3
Romain MARCA <romain.marca@sii.fr>
2019-05-14 14:21:55 +0200

    finish game on web

--- src/Boot.js
+++ src/Boot.js
@@ -1,10 +1,10 @@
 let Ball = {
-    _WIDTH: 380,
+    _WIDTH: 320,
     _HEIGHT: 480
     };
 
-Ball.boot = function(game) {};
-Ball.boot.prototype = {
+Ball.Boot = function(game) {};
+Ball.Boot.prototype = {
     preload: function() {
         this.load.image('preloaderBg', 'img/loading-bg.png');
         this.load.image('preloaderBar', 'img/loading-bar.png');
--- src/Howto.js
+++ src/Howto.js
@@ -1,7 +1,7 @@
 Ball.Howto = function(game) {};
 Ball.Howto.prototype = {
     create: function() {
-        this.buttonContinue = this.add.button(, , 'screen-howtoplay', this.startGame, this);
+        this.buttonContinue = this.add.button(0, 0, 'screen-howtoplay', this.startGame, this);
     },
     startGame: function() {
         this.game.state.start('Game');
